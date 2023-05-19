48628e37376fe86226665a943ed96ebadf637c69
lucas-possato-dev <lucaspstinho@gmail.com>
2023-05-19 11:17:59 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import HouseContextProvider from "./components/HouseContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <HouseContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </HouseContextProvider>
   </React.StrictMode>
 );
